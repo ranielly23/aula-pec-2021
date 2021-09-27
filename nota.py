757ff2a670623ddc8bde3761c5eca89a4222db12
Francisca Ranielly <franciscaranielly7@gmail.com>
2021-09-27 18:52:50 -0300

    Adicionei funçao main()

--- nota.py
+++ nota.py
@@ -1,5 +1,10 @@
 def desconto (dias):
     return dias * 5
-d = int(input("Digite os dias de atraso: "))
-desc = desconto(d)
-print(f'Você deve descontar {desc} pontos da nota,')
+
+def main():
+    d = int(input("Digite os dias de atraso: "))
+    desc = desconto(d)
+    print(f'Você deve descontar {desc} pontos da nota,')
+
+if __name__ == '__main__':
+    main()
